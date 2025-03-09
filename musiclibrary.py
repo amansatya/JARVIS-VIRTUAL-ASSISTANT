0b3c461ce4e941e8b63e18f138795f2a836d6617
amansatya <satyaaman1012@gmail.com>
2025-03-09 22:23:50 +0530

    UPDATED SONG NAMES

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,7 +1,7 @@
 music = {
-    "bye byr": "https://youtu.be/Eo-KmOd3i7s?si=oR_zRR91fYyBbX5_",
+    "bye bye": "https://youtu.be/Eo-KmOd3i7s?si=oR_zRR91fYyBbX5_",
     "faded": "https://youtu.be/60ItHLz5WEA?si=t2MPq9mk7tnbxo7k",
-    "beliver": "https://youtu.be/7wtfhZwyrcc?si=wr5TONaKV-AJe5Nn",
+    "believer": "https://youtu.be/7wtfhZwyrcc?si=wr5TONaKV-AJe5Nn",
     "pneuma": "https://youtu.be/5ClCaPmAA7s?si=BPFWu-kgdGaDnWF6",
     "paradise": "https://youtu.be/1G4isv_Fylg?si=6cQC5eimtVUEnTIM"
 }
